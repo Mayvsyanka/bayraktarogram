0ff0e7923c4a28ddb78c62ad63a1d649b8e0d57d
Marinakrash <marinakrash@gmail.com>
2023-07-23 16:54:51 +0300

    1st commit
    1st commit

---
+++ src/routes/comments.py
@@ -0,0 +1,51 @@
+from typing import List
+
+from fastapi import APIRouter, HTTPException, Depends, status
+from sqlalchemy.orm import Session
+
+from src.database.db import get_db
+from src.schemas import CommentModel, CommentResponse
+from src.repository import comments as repository_comments
+
+router = APIRouter(prefix='/comments', tags=["comments"])
+
+
+@router.get("/", response_model=List[CommentResponse])
+async def read_comments(skip: int = 0, limit: int = 100, db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    comments = await repository_comments.get_comments(skip, limit, current_user, db)
+    return comments
+
+
+@router.get("/{comment_id}", response_model=CommentResponse)
+async def read_comment(comment_id: int, db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    comment = await repository_comments.get_comment(comment_id, current_user, db)
+    if comment is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Comment not found")
+    return comment
+
+
+@router.post("/", response_model=CommentResponse)
+async def create_comment(body: CommentModel, db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    return await repository_comments.create_comment(body, current_user, db)
+
+
+@router.put("/{comment_id}", response_model=CommentResponse)
+async def update_comment(body: CommentModel, comment_id: int, db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    comment = await repository_comments.update_comment(comment_id, current_user, body, db)
+    if comment is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Comment not found")
+    return comment
+
+
+@router.delete("/{comment_id}", response_model=CommentResponse)
+async def remove_comment(comment_id: int, db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    comment = await repository_comments.remove_comment(comment_id, current_user, db)
+    if comment is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Comment not found")
+    return comment
+
--- src/schemas.py
+++ src/schemas.py
@@ -1,7 +1,25 @@
 import enum
+from datetime import datetime
+from pydantic import BaseModel, Field
 
 class Role (enum.Enum):
     admin: str = 'admin'
     moderator: str = 'moderator'
     user: str = 'user'
 
+
+class CommentModel(BaseModel):
+    content: str = Field(max_length=250)
+    created_at: datetime
+    updated_at: datetime
+    user_id: int
+    user_role: str = Field(max_length=50)
+    photo_id = int
+
+
+class CommentResponse(CommentModel):
+    id: int
+    #user_id: int
+
+    class Config:
+        orm_mode = True
